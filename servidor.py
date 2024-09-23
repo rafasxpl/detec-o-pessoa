380d1d801f0343bd663bd56808685327dd7eb103
Rafael Lopes <rl1024214@gmail.com>
2024-09-23 19:15:11 -0300

    atualização

--- servidor.py
+++ servidor.py
@@ -24,16 +24,15 @@ def detection():
             confidence = round(row['confidence'] * 100, 0)
             class_id = int(row['class'])
 
-            if class_id == 0:  # Se for uma pessoa
-                cv.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
+            if class_id == 0:  # Identifica apenas pessoas (classe com id 0)
+                cv.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2) #desenha o retangulo da caixa delimitadora
                 cv.putText(frame, f'Pessoa {confidence}%', (box[0], box[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
         # Codifica o frame em JPEG
         ret, buffer = cv.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
-        # Retorna o frame como parte do fluxo multipart
-        
+        # Retorna o frame em partes pra enviar pro flask renderizar - função video_feed()
         yield (b'--frame\r\n'
        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -49,5 +48,4 @@ def video_feed():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# Libera a câmera após o término do uso
 cam.release()
--- yolo.py
+++ yolo.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import torch #lib que carrega o YOLO
+import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 cam = cv.VideoCapture(0)
@@ -11,31 +11,24 @@ def detection():
         if not ret:
             break
 
-        results = model(frame) #processa e detecta os objetos da webcam
+        results = model(frame)
         pred = results.pandas().xyxy[0]
         
         for index, row in pred.iterrows():
-            box = [int(x) for x in row[['xmin', 'ymin', 'xmax', 'ymax']]]  # retorna as coordenada x e y iniciais da caixa delimitadora, assim como os x e y finais
+            box = [int(x) for x in row[['xmin', 'ymin', 'xmax', 'ymax']]]
+            confidence = round(row['confidence'] * 100, 0)
+            class_id = int(row['class'])
 
-            confidence = row['confidence'] * 100 # confiança (em porcentagem) da detecção 
-            confidence = round(confidence, 0) # retira as casas decimais
-            class_id = int(row['class']) # retorna o id da classe classe detectada 
-
-            if class_id == 0:
+            if class_id == 0: 
                 cv.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
-                cv.putText(frame, f'pessoa {confidence}%', (box[0], box[1] - 20), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    
-            # buffer = cv.imencode('.jpg', frame)
-            # data_encode = np.array(buffer) 
-            # frame = data_encode.tobytes()
-            # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                cv.putText(frame, f'Pessoa {confidence}%', (box[0], box[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-        cv.imshow('detection', frame)
-
+            cv.imshow("sla", frame)
+    #     ret, buffer = cv.imencode('.jpg', frame)
+    #     frame = buffer.tobytes()
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cam.release()
-    cv.destroyAllWindows()
-
+    #     yield (b'--frame\r\n'
+    #    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
 detection()
